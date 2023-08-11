b5adaf54e4310563dab75bedf3387420dfe302b8
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-11 17:25:02 +0300

    changed NoteOperation.py as created search_by_title

--- NoteOperation.py
+++ NoteOperation.py
@@ -14,3 +14,25 @@ def create_note():
     print("-------------------------------------------")
     print(f"Заметка с наименованием {title} создана")
     return Note.Note(title=title, body=body)
+
+def search_by_title(path):
+    searching_title = input("Введите название искомой заметки:\n")
+    list_note = File_operation.read_file(path)
+    count = 0
+    el_index = 0
+    for element in list_note:
+        if element["Название заметки"] == searching_title:
+            count += 1
+            el_index = list_note.index(element)
+    if count == 0:
+        print("Не найдено заметок с заданным названием")
+    elif count == 1:
+        print("------------------------------")
+        print(f"Было найдена заметка с названием {searching_title}")
+        File_operation.output_element_to_console(list_note[el_index])
+    else:
+        print(f"Было найдено {count} заметок с названием '{searching_title}'")
+        for element in list_note:
+            if element["Название заметки"] == searching_title:
+                print(element["ID"] + " " + element["Название заметки"])
+        search_by_id(path)
