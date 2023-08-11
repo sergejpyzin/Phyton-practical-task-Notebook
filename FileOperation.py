47927be7db8c2a45b9c2716d98cfde08026bfa18
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-11 17:40:45 +0300

    changed FileOperation.py as modified according to PEP 8

--- FileOperation.py
+++ FileOperation.py
@@ -21,25 +21,28 @@ def is_accessible(path, mode="r"):
     try:
         file = open(path, mode)
         file.close()
-    except:
+    except FileNotFoundError:
         return False
     return True
+
 
 def read_file(path):
     if file_exists(path):
         with open(path) as file:
             return json.load(file)
 
+
 def write_note_in_file(path):
     if file_is_empty(path):
         with open(path) as fr:
-            listObj = json.load(fr)
-        listObj.insert(0, NoteOperation.create_note().to_dict())
+            list_obj = json.load(fr)
+        list_obj.insert(0, NoteOperation.create_note().to_dict())
     else:
-        listObj = []
-        listObj.insert(0, NoteOperation.create_note().to_dict())
+        list_obj = []
+        list_obj.insert(0, NoteOperation.create_note().to_dict())
     with open(path, "w") as fr:
-        json.dump(listObj, fr, ensure_ascii=False, indent=2)
+        json.dump(list_obj, fr, ensure_ascii=False, indent=2)
+
 
 def create_file(path):
     try:
@@ -47,6 +50,7 @@ def create_file(path):
         file.close()
     except FileNotFoundError:
         print("Создать файл не удалось")
+
 
 def output_list_note_to_console(path):
     list_note = read_file(path)
@@ -56,5 +60,6 @@ def output_list_note_to_console(path):
 
 def output_element_to_console(element):
     print(f'ID {element["ID"]}, Заметка {element["Название заметки"]}\n '
-          f'\tТекст заметки: {element["Содержание заметки"]}\n\tДата создания/изменения: {element["Дата создания/изменения"]}, '
+          f'\tТекст заметки: {element["Содержание заметки"]}'
+          f'\n\tДата создания/изменения: {element["Дата создания/изменения"]}, '
           f'Время создания/изменения {element["Время создания/изменения"]};')
