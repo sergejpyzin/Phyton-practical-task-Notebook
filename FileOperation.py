b87603e77e08067d5f26958c77483eca92c1c044
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-11 17:32:37 +0300

    changed FileOperation.py as added boolean def file_is_empty, def file_exists, def is_accessible

---
+++ FileOperation.py
@@ -0,0 +1,23 @@
+import os
+
+
+def file_is_empty(path):
+    if file_exists(path):
+        return os.stat(path).st_size != 0
+
+
+def file_exists(path):
+    try:
+        os.stat(path)
+    except OSError:
+        return False
+    return True
+
+
+def is_accessible(path, mode="r"):
+    try:
+        file = open(path, mode)
+        file.close()
+    except:
+        return False
+    return True
