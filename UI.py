8c281dfa11281c2d07e5d83251b5165a25d99f63
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-11 17:54:16 +0300

    created UI.py as added def choice_menu

--- UI.py
+++ UI.py
@@ -24,3 +24,24 @@ def menu():
         print("\033[3m\033[31m{}\033[0m".format("Файла для сохранения заметок несуществует! Создайте хотя бы одну заметку."))
         NoteOperation.create_note()
         choice_menu()
+
+def choice_menu():
+    print("========================================================")
+    print("Для продолжения выберете пункт меню\n1 - создание заметки\n2 - редактирование заметки\n3 - поиск заметки\n"
+          "4 - удаление заметки\n5 - вывести все заметки\n6 - выход в главное меню")
+    answer_from_user = input("Введите номер выбранного пункта меню:\n").strip(" \n\r")
+    if answer_from_user == "1":
+        create_menu()
+    elif answer_from_user == "2":
+        change_note_menu()
+    elif answer_from_user == "3":
+        search_menu()
+    elif answer_from_user == "4":
+        remove_menu()
+    elif answer_from_user == "5":
+        read_note_menu()
+    elif answer_from_user == "6":
+        greeting()
+    else:
+        print("\033[3m\033[31m{}\033[0m".format("Введённый номер пукта меню несуществует! Попробуйте ещё раз."))
+        choice_menu()
