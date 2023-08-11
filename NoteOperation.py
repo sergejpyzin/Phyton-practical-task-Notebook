8e6e305c6783448385f6b87ec9ff7051822c0e9e
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-08-11 17:24:00 +0300

    changed NoteOperation.py as created create_note

--- NoteOperation.py
+++ NoteOperation.py
@@ -1,6 +1,16 @@
+import Note
+
+
 def check_length_message(message):
     while len(message) == 0:
         print("Сообщение должно содержать, как минимум, один символ!")
         message = input(" Попробуйте еще раз. ")
     else:
         return message
+
+def create_note():
+    title = check_length_message(input("Введите название заметки: "))
+    body = check_length_message(input("Введите содержание заметки: "))
+    print("-------------------------------------------")
+    print(f"Заметка с наименованием {title} создана")
+    return Note.Note(title=title, body=body)
